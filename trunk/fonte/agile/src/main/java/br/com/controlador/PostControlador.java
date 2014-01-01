01b6145e5b9867720daa28356e0dd2da0d22834a
claudemirramosferreira <claudemirramosferreira@fa381899-0fff-0e4c-82c0-ba291e640112>
2014-01-01 16:45:27 +0000

    add primeface

---
+++ trunk/fonte/agile/src/main/java/br/com/controlador/PostControlador.java
@@ -0,0 +1,14 @@
+package br.com.controlador;
+
+import javax.faces.bean.ManagedBean;
+
+
+@ManagedBean
+public class PostControlador {
+	
+	public void init(){
+		
+		System.out.println("alooooooooooooo");
+	}
+
+}
