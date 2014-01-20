69bb2e654f6f2b0c1f33f50218632d75eeb9828c
claudemirramosferreira <claudemirramosferreira@fa381899-0fff-0e4c-82c0-ba291e640112>
2014-01-20 03:00:26 +0000

    inclusao do novo layout

--- trunk/fonte/ieadam/src/main/java/br/com/ieadam/dominio/Area.java
+++ trunk/fonte/ieadam/src/main/java/br/com/ieadam/dominio/Area.java
@@ -1,5 +1,101 @@
 package br.com.ieadam.dominio;
 
+import java.util.Collection;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="ieadam_area")
 public class Area {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="id_area")
+	private int id;
+	
+	@OneToOne
+	private Membro pastor;
+	
+	@ManyToOne
+	private Nucleo nucleo;
+	
+	private String nome;
+	
+	@Enumerated(EnumType.STRING)
+	private Situacao situacao;
+	
+	@Temporal(TemporalType.DATE)
+	private Date dataUltimaAtualizacao;
+
+	@OneToMany(mappedBy="area")
+	private Collection<Congregacao> congregacoes;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Membro getPastor() {
+		return pastor;
+	}
+
+	public void setPastor(Membro pastor) {
+		this.pastor = pastor;
+	}
+
+	public Nucleo getNucleo() {
+		return nucleo;
+	}
+
+	public void setNucleo(Nucleo nucleo) {
+		this.nucleo = nucleo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Situacao getSituacao() {
+		return situacao;
+	}
+
+	public void setSituacao(Situacao situacao) {
+		this.situacao = situacao;
+	}
+
+	public Date getDataUltimaAtualizacao() {
+		return dataUltimaAtualizacao;
+	}
+
+	public void setDataUltimaAtualizacao(Date dataUltimaAtualizacao) {
+		this.dataUltimaAtualizacao = dataUltimaAtualizacao;
+	}
+
+	public Collection<Congregacao> getCongregacoes() {
+		return congregacoes;
+	}
+
+	public void setCongregacoes(Collection<Congregacao> congregacoes) {
+		this.congregacoes = congregacoes;
+	}
 }
--- trunk/fonte/ieadam/src/main/java/br/com/ieadam/dominio/Zona.java
+++ trunk/fonte/ieadam/src/main/java/br/com/ieadam/dominio/Zona.java
@@ -1,5 +1,93 @@
 package br.com.ieadam.dominio;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="ieadam_zona")
 public class Zona {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="id_zona")
+	private int id;
+	
+	@Temporal(TemporalType.DATE)
+	private Date dataUltimaAtualizacao;
+	
+	private String nome;
+	private String situacao;
+	
+	@OneToOne
+	private Membro primeiroResponsavel;
+	
+	@OneToOne
+	private Membro segundoResponsavel;
+	
+	private int cd_ZonaArgo;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getDataUltimaAtualizacao() {
+		return dataUltimaAtualizacao;
+	}
+
+	public void setDataUltimaAtualizacao(Date dataUltimaAtualizacao) {
+		this.dataUltimaAtualizacao = dataUltimaAtualizacao;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSituacao() {
+		return situacao;
+	}
+
+	public void setSituacao(String situacao) {
+		this.situacao = situacao;
+	}
+
+	public Membro getPrimeiroResponsavel() {
+		return primeiroResponsavel;
+	}
+
+	public void setPrimeiroResponsavel(Membro primeiroResponsavel) {
+		this.primeiroResponsavel = primeiroResponsavel;
+	}
+
+	public Membro getSegundoResponsavel() {
+		return segundoResponsavel;
+	}
+
+	public void setSegundoResponsavel(Membro segundoResponsavel) {
+		this.segundoResponsavel = segundoResponsavel;
+	}
+
+	public int getCd_ZonaArgo() {
+		return cd_ZonaArgo;
+	}
+
+	public void setCd_ZonaArgo(int cd_ZonaArgo) {
+		this.cd_ZonaArgo = cd_ZonaArgo;
+	}
 }
