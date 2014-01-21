ee9623dee41bf91a08c8b4b5a8923bed05fe715d
claudemirramosferreira <claudemirramosferreira@fa381899-0fff-0e4c-82c0-ba291e640112>
2014-01-21 17:59:06 +0000

    teste na impressao do relatorio

--- trunk/fonte/ieadam/src/main/java/br/com/ieadam/controle/RelatorioSecretariaControlador.java
+++ trunk/fonte/ieadam/src/main/java/br/com/ieadam/controle/RelatorioSecretariaControlador.java
@@ -1,9 +1,24 @@
 package br.com.ieadam.controle;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 import org.springframework.security.core.context.SecurityContextHolder;
 
@@ -16,25 +31,70 @@ import br.com.ieadam.servico.RelatorioSecretariaServico;
 public class RelatorioSecretariaControlador {
 
 	private FiltroRelatorioDTO filtroRelatorioDTO;
-	
+
 	@ManagedProperty(value = "#{relatorioSecretariaServicoImpl}")
 	private RelatorioSecretariaServico relatorioSecretariaServico;
-	
+
+	private List<Usuario> usuarios = new ArrayList<Usuario>();
+
 	@PostConstruct
 	public void init() {
 		this.filtroRelatorioDTO = new FiltroRelatorioDTO();
-		this.filtroRelatorioDTO.setUsuarioLogado(
-				(Usuario)SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+		this.filtroRelatorioDTO
+				.setUsuarioLogado((Usuario) SecurityContextHolder.getContext()
+						.getAuthentication().getPrincipal());
+
+		Usuario usuario = new Usuario();
+		usuario.setLogin("teste");
+
+		this.usuarios.add(usuario);
+	}
+
+	public void imprimir() {
+		try {
+
+			ExternalContext externalContext = FacesContext.getCurrentInstance()
+					.getExternalContext();
+			ServletContext context = (ServletContext) externalContext
+					.getContext();
+			FacesContext facesContext = FacesContext.getCurrentInstance();
+			String arquivo = context
+					.getRealPath("/WEB-INF/jasper/teste.jasper");
+			System.out.println(arquivo);
+
+			FacesContext fc = FacesContext.getCurrentInstance();
+			HttpServletResponse response = (HttpServletResponse) fc
+					.getExternalContext().getResponse();
+
+			response.setContentType("application/pdf");
+			response.addHeader("Content-disposition",
+					"attachment; filename=\"teste.pdf\"");
+
+			InputStream inputStream = new FileInputStream(new File(arquivo));
+
+			JasperPrint impressao = JasperFillManager.fillReport(inputStream,
+					null, new JRBeanCollectionDataSource(this.usuarios, false));
+
+			JasperExportManager.exportReportToPdfStream(impressao,
+					response.getOutputStream());
+
+			facesContext.responseComplete();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Erro: " + e.getMessage());
+		}
 	}
 
 	public void gerarRelatorio() {
-		this.relatorioSecretariaServico.gerarRelatorioSecretaria(this.filtroRelatorioDTO);
+		this.relatorioSecretariaServico
+				.gerarRelatorioSecretaria(this.filtroRelatorioDTO);
 	}
-	
+
 	public String relatorioSecretaria() {
 		return "/pages/relatorio/secretaria/pesquisa.jsf";
 	}
-	
+
 	public FiltroRelatorioDTO getFiltroRelatorioDTO() {
 		return filtroRelatorioDTO;
 	}
@@ -50,5 +110,13 @@ public class RelatorioSecretariaControlador {
 	public void setRelatorioSecretariaServico(
 			RelatorioSecretariaServico relatorioSecretariaServico) {
 		this.relatorioSecretariaServico = relatorioSecretariaServico;
+	}
+
+	public List<Usuario> getUsuarios() {
+		return usuarios;
+	}
+
+	public void setUsuarios(List<Usuario> usuarios) {
+		this.usuarios = usuarios;
 	}
 }
