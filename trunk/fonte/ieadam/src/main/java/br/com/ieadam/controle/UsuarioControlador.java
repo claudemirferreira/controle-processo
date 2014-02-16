ed3525e7b278cebd72a488481f7f4487e606c65a
claudemirramosferreira <claudemirramosferreira@fa381899-0fff-0e4c-82c0-ba291e640112>
2014-02-16 02:46:38 +0000

    ADD CRUD DO USUARIO

--- trunk/fonte/ieadam/src/main/java/br/com/ieadam/controle/UsuarioControlador.java
+++ trunk/fonte/ieadam/src/main/java/br/com/ieadam/controle/UsuarioControlador.java
@@ -19,24 +19,34 @@ import br.com.ieadam.dominio.Sistema;
 import br.com.ieadam.dominio.Usuario;
 import br.com.ieadam.servico.MembroServico;
 import br.com.ieadam.servico.SistemaServico;
+import br.com.ieadam.servico.UsuarioServico;
 import br.com.ieadam.utils.IEADAMAuthenticationManager;
 
 @ManagedBean
 @SessionScoped
 public class UsuarioControlador {
 
+	private Usuario entidade;
+
+	private Usuario pesquisa;
+
 	private Usuario usuario;
-	
+
 	private Sistema sistema;
 
+	private List<Usuario> lista;
+
 	private List<Perfil> perfis = new ArrayList<Perfil>();
+
+	@ManagedProperty(value = "#{usuarioServicoImpl}")
+	private UsuarioServico servico;
 
 	@ManagedProperty(value = "#{paginaCentralControladorBean}")
 	private PaginaCentralControladorBean paginaCentralControladorBean;
 
 	@ManagedProperty(value = "#{membroServicoImpl}")
 	private MembroServico membroServico;
-	
+
 	@ManagedProperty(value = "#{sistemaServicoImpl}")
 	private SistemaServico sistemaServico;
 
@@ -45,12 +55,86 @@ public class UsuarioControlador {
 	@ManagedProperty(value = "#{IEADAMAuthenticationManager}")
 	private IEADAMAuthenticationManager IEADAMAuthenticationManager;
 
+	@ManagedProperty(value = "#{paginaCentralControlador}")
+	private PaginaCentralControladorBean paginaCentralControlador;
+
+	private final String TELA_CADASTRO = "paginas/usuario/cadastro.xhtml";
+	private final String TELA_PESQUISA = "paginas/usuario/pesquisa.xhtml";
+
 	@PostConstruct
 	public void init() {
 		this.usuario = new Usuario();
-		
-		this.sistema = sistemaServico.findByCodigo("IEADAM"); 
-		
+		this.entidade = new Usuario();
+		this.pesquisa = new Usuario();
+		this.sistema = sistemaServico.findByCodigo("IEADAM");
+
+	}
+
+	public void pesquisar() {
+		// this.lista = this.servico.findByNomeLike(this.pesquisa.getNome());
+	}
+
+	public void detalhe(Usuario usuario) {
+		this.entidade = usuario;
+		this.telaCadastro();
+
+	}
+
+	public void salvar() {
+
+		this.servico.salvar(this.entidade);
+		this.lista = servico.listarTodos();
+		this.telaPesquisa();
+	}
+
+	public void excluir(Usuario usuario) {
+		this.servico.remover(usuario);
+		this.lista = servico.listarTodos();
+	}
+
+	public void novo() {
+		this.entidade = new Usuario();
+		this.telaCadastro();
+	}
+
+	public void telaCadastro() {
+		this.paginaCentralControlador.setPaginaCentral(this.TELA_CADASTRO);
+	}
+
+	public void telaPesquisa() {
+		this.paginaCentralControlador.setPaginaCentral(this.TELA_PESQUISA);
+	}
+
+	public Usuario getEntidade() {
+		return entidade;
+	}
+
+	public void setEntidade(Usuario entidade) {
+		this.entidade = entidade;
+	}
+
+	public Usuario getPesquisa() {
+		return pesquisa;
+	}
+
+	public void setPesquisa(Usuario pesquisa) {
+		this.pesquisa = pesquisa;
+	}
+
+	public List<Usuario> getLista() {
+		return lista;
+	}
+
+	public void setLista(List<Usuario> lista) {
+		this.lista = lista;
+	}
+
+	public UsuarioServico getServico() {
+		return servico;
+	}
+
+	public void setServico(UsuarioServico servico) {
+		this.servico = servico;
 	}
 
 	public String logar() {
@@ -74,8 +158,8 @@ public class UsuarioControlador {
 			this.usuario = (Usuario) SecurityContextHolder.getContext()
 					.getAuthentication().getPrincipal();
 
-			this.colunas =3;
-//					Util					.definirTamanhoColuna(usuario.getPerfis().size());
+			this.colunas = 3;
+			// Util .definirTamanhoColuna(usuario.getPerfis().size());
 
 			return "/index.xhtml?faces-redirect=true";
 		}
@@ -155,5 +239,14 @@ public class UsuarioControlador {
 
 	public void setSistemaServico(SistemaServico sistemaServico) {
 		this.sistemaServico = sistemaServico;
+	}
+
+	public PaginaCentralControladorBean getPaginaCentralControlador() {
+		return paginaCentralControlador;
+	}
+
+	public void setPaginaCentralControlador(
+			PaginaCentralControladorBean paginaCentralControlador) {
+		this.paginaCentralControlador = paginaCentralControlador;
 	}
 }
