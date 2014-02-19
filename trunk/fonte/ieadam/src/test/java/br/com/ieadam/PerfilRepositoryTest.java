ce4271f3fa5409f135589d978dfe8225cb077749
claudemirramosferreira <claudemirramosferreira@fa381899-0fff-0e4c-82c0-ba291e640112>
2014-02-19 17:28:42 +0000

    add class test

--- trunk/fonte/ieadam/src/test/java/br/com/ieadam/PerfilRepositoryTest.java
+++ trunk/fonte/ieadam/src/test/java/br/com/ieadam/PerfilRepositoryTest.java
@@ -1,5 +1,8 @@
 package br.com.ieadam;
 
+import java.util.List;
+
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,31 +10,30 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import br.com.ieadam.dominio.Perfil;
-import br.com.ieadam.dominio.Sistema;
+import br.com.ieadam.dominio.Usuario;
 import br.com.ieadam.repositorio.PerfilRepositorio;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath*:META-INF/spring/applicationContext.xml")
+// @ContextConfiguration(locations="classpath*:META-INF/spring/test-context.xml")
+@Ignore
 public class PerfilRepositoryTest {
 
 	@Autowired
 	PerfilRepositorio repository;
 
-	
 	@Test
-	public void testInsert() {
-		
-		Sistema sistema = new Sistema(); 
-		sistema.setIdSistema(1L);
-		
-		Perfil perfil = new Perfil();
-		perfil.setImagem("imagem");
-		perfil.setNome("Perfil Teste");
-		perfil.setSistema(sistema);
+	public void test() {
 
-		repository.saveAndFlush(perfil);
-		
+		Usuario u = new Usuario();
+
+//		List<Perfil> perfis = repository.listarPerfilPorUsuario(2);
+
+		// assertNotNull(perfis);
+
+		// Perfil dbperfil = repository.findOne(perfil.getPerfilId());
+		// assertNotNull(dbperfil);
+		// System.out.println(dbperfil.getTitle());
 	}
-	
-	
+
 }
--- trunk/fonte/ieadam/src/test/java/br/com/ieadam/UsuarioRepositoryTest.java
+++ trunk/fonte/ieadam/src/test/java/br/com/ieadam/UsuarioRepositoryTest.java
@@ -7,64 +7,47 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import br.com.ieadam.dominio.NivelPastoral;
 import br.com.ieadam.dominio.Usuario;
 import br.com.ieadam.repositorio.UsuarioRepositorio;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath*:META-INF/spring/applicationContext.xml")
 // @ContextConfiguration(locations="classpath*:META-INF/spring/test-context.xml")
+@Ignore
 public class UsuarioRepositoryTest {
 
 	@Autowired
 	UsuarioRepositorio repository;
-	
-	@Test
-	public void testInsert() {
-		Usuario usuario = new Usuario();
-		usuario.setLogin("teste");
-		usuario.setSenha("teste");
-		usuario.setNome("TESTE");
-		usuario.setStatus("A");
 
-		repository.saveAndFlush(usuario);
-		
-	}
-	
-	/**
-	 * 
-	 */
-	@Ignore
 	@Test
 	public void test() {
-//		Usuario usuario = new Usuario();
-//		usuario.setLogin("pastorzona");
-//		usuario.setSenha("pastorzona");
-//
-//		Membro m = membroRepository.findOne(7);
-//
-//		usuario.setMembro(m);
-//		usuario.setSituacao("a");
-//
-//		List<Perfil> list = new ArrayList<Perfil>();
-//		Perfil p = new Perfil();
-//
-//		p.setId(1);
-//		list.add(p);
-//		
-//		p = new Perfil();
-//		p.setId(2);
-//		list.add(p);
-//
-//		usuario.setPerfis(list);
-		
-		Usuario u = repository.findOne(16L);
-		
-		NivelPastoral nivel = new NivelPastoral();
-		nivel.setId(5);
-//		u.setNivelPastoral(nivel);
+		// Usuario usuario = new Usuario();
+		// usuario.setLogin("pastorzona");
+		// usuario.setSenha("pastorzona");
+		//
+		// Membro m = membroRepository.findOne(7);
+		//
+		// usuario.setMembro(m);
+		// usuario.setSituacao("a");
+		//
+		// List<Perfil> list = new ArrayList<Perfil>();
+		// Perfil p = new Perfil();
+		//
+		// p.setId(1);
+		// list.add(p);
+		//
+		// p = new Perfil();
+		// p.setId(2);
+		// list.add(p);
+		//
+		// usuario.setPerfis(list);
 
-		repository.saveAndFlush(u);
+//		Usuario u = repository.findOne(16);
+
+//		nivel.setId(5);
+		// u.setNivelPastoral(nivel);
+
+//		repository.saveAndFlush(u);
 
 		// Usuario dbpost = repository.findOne(usuario.getId());
 		// assertNotNull(dbpost);
