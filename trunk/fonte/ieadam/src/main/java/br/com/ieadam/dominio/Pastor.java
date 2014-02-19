8dd138045953667c6e88dee4d3f8d7cbb64fbc85
claudemirramosferreira <claudemirramosferreira@fa381899-0fff-0e4c-82c0-ba291e640112>
2014-02-19 11:57:26 +0000

    add pastor

---
+++ trunk/fonte/ieadam/src/main/java/br/com/ieadam/dominio/Pastor.java
@@ -0,0 +1,61 @@
+package br.com.ieadam.dominio;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ieadam_pastor")
+public class Pastor extends AbstractEntity implements Serializable {
+
+	private static final long serialVersionUID = 7025896792570044669L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(nullable = false, length = 60)
+	private String nome;
+
+	@OneToOne
+	@JoinColumn(name = "id_usuario", referencedColumnName = "id_usuario")
+	private Usuario usuario;
+
+	// @OneToOne
+	// @JoinColumn(name = "membro_id", referencedColumnName = "id")
+	// private Membro membro;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	
+
+}
