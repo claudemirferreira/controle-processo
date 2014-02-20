e3a08fd21cea2a0d583735bfc481dfefaf64a729
claudemirramosferreira@gmail.com <claudemirramosferreira@gmail.com@fa381899-0fff-0e4c-82c0-ba291e640112>
2014-02-20 04:42:19 +0000

    inicio da lista de perfis com picklist

--- trunk/fonte/ieadam/src/main/java/br/com/ieadam/repositorio/UsuarioPerfilRepositorio.java
+++ trunk/fonte/ieadam/src/main/java/br/com/ieadam/repositorio/UsuarioPerfilRepositorio.java
@@ -1,12 +1,26 @@
 package br.com.ieadam.repositorio;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import br.com.ieadam.dominio.Usuario;
 import br.com.ieadam.dominio.UsuarioPerfil;
 
 @Repository
 public interface UsuarioPerfilRepositorio extends
 		JpaRepository<UsuarioPerfil, Long> {
+	
+
+//	@Query("select up from UsuarioPerfil up " +
+//			" where up.usuario = :usuario " +
+//			" and up NOT IN ( " +
+//				" select up2 from UsuarioPerfil up2 " + 
+//				" where up2 = up " +
+//			" ) ")
+//	public List<UsuarioPerfil> listaPerfisDiferentesUsuario(/*@Param("usuario")*/ Usuario usuario);
 
 }
