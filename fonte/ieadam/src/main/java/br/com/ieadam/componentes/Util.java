7b2d8a66d2d8b29335eab2d9a2cad7603b35803a
claudemirramosferreira <claudemirramosferreira@fa381899-0fff-0e4c-82c0-ba291e640112>
2014-05-08 17:43:29 +0000

    add pagina de alteracao de senha

--- fonte/ieadam/src/main/java/br/com/ieadam/componentes/Util.java
+++ fonte/ieadam/src/main/java/br/com/ieadam/componentes/Util.java
@@ -1,18 +1,57 @@
 package br.com.ieadam.componentes;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
+
 public class Util {
-	
-	public static int definirTamanhoColuna(int tamanho){
-		
-		if (tamanho == 3) {
+
+	public static int definirTamanhoColuna(int tamanho) {
+
+		if (tamanho < 4) {
 			return tamanho;
 		} else {
 			double col = Math.sqrt(tamanho);
 			int colunas = (int) col;
-			if(colunas  < col)
-				colunas ++;
+			if (colunas < col)
+				colunas++;
 			return colunas;
 		}
+	}
+
+	public static byte[] getBytes(InputStream is) throws IOException {
+		int len;
+		int size = 1024;
+		byte[] buf;
+
+		if (is instanceof ByteArrayInputStream) {
+			size = is.available();
+			buf = new byte[size];
+			len = is.read(buf, 0, size);
+		} else {
+			ByteArrayOutputStream bos = new ByteArrayOutputStream();
+			buf = new byte[size];
+			while ((len = is.read(buf, 0, size)) != -1)
+				bos.write(buf, 0, len);
+			buf = bos.toByteArray();
+		}
+		return buf;
+	}
+
+	public static StreamedContent converterBytesToStreamedContent(byte[] bytes) {
+		if (bytes != null) {
+			InputStream is = new ByteArrayInputStream(bytes);
+			System.out.println("size file : " + bytes.length);
+			StreamedContent image = new DefaultStreamedContent(is);
+			System.out.println("dans le convertisseur : "
+					+ image.getContentType());
+			return image;
+		} else
+			return null;
 	}
 
 }
