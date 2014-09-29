41d1b2cd8d096524545abbf5bed7b604a0c92e27
claudemirramosferreira <claudemirramosferreira@gmail.com>
2014-09-29 21:10:19 +0000

    aa

--- fonte/ieadam/src/main/java/br/com/ieadam/servico/CongregacaoServico.java
+++ fonte/ieadam/src/main/java/br/com/ieadam/servico/CongregacaoServico.java
@@ -2,6 +2,7 @@ package br.com.ieadam.servico;
 
 import java.util.List;
 
+import br.com.ieadam.dominio.Area;
 import br.com.ieadam.dominio.Congregacao;
 
 public interface CongregacaoServico {
@@ -11,5 +12,7 @@ public interface CongregacaoServico {
 	public Congregacao salvar(Congregacao congregacao);
 
 	public void remover(Congregacao congregacao);
+
+	public List<Congregacao> findByArea(Area area);
 
 }
