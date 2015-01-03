1178f7a2424d387cb5fd015c8316a5fe90a2786f
claudemirramosferreira@gmail.com <claudemirramosferreira@gmail.com@fa381899-0fff-0e4c-82c0-ba291e640112>
2015-01-03 17:36:54 +0000

    corrigido problema com parametros de ano.

--- trunk/fonte/ieadam/src/main/java/br/com/ieadam/dominio/ViewMembro.java
+++ trunk/fonte/ieadam/src/main/java/br/com/ieadam/dominio/ViewMembro.java
@@ -36,7 +36,46 @@ public class ViewMembro {
 
 	@Column
 	private String zona;
-
+	
+//	@Column
+//	private Date dataNascimento;
+//	
+//	@Column
+//	private String funcao;
+//
+//	@Column
+//	private String congregacao;
+//
+//	@Column
+//	private String pastor;
+//	
+//	@Column
+//	private String estadoCivil;
+//
+//	@Column
+//	private String conjuge;
+//	
+//	@Column
+//	private String recebidoPor;
+//
+//	@Column
+//	private String dataBatismo;
+//
+//	@Column
+//	private String procedencia;
+//
+//	@Column
+//	private String situacao;
+//
+//	@Column
+//	private String endereco;
+//
+//	@Column
+//	private String bairro;
+//
+//	@Column
+//	private String telefone;
+	
 	public int getIdMembro() {
 		return idMembro;
 	}
@@ -100,4 +139,108 @@ public class ViewMembro {
 	public void setZona(String zona) {
 		this.zona = zona;
 	}
+
+//	public Date getDataNascimento() {
+//		return dataNascimento;
+//	}
+//
+//	public void setDataNascimento(Date dataNascimento) {
+//		this.dataNascimento = dataNascimento;
+//	}
+//
+//	public String getFuncao() {
+//		return funcao;
+//	}
+//
+//	public void setFuncao(String funcao) {
+//		this.funcao = funcao;
+//	}
+//
+//	public String getCongregacao() {
+//		return congregacao;
+//	}
+//
+//	public void setCongregacao(String congregacao) {
+//		this.congregacao = congregacao;
+//	}
+//
+//	public String getPastor() {
+//		return pastor;
+//	}
+//
+//	public void setPastor(String pastor) {
+//		this.pastor = pastor;
+//	}
+//
+//	public String getEstadoCivil() {
+//		return estadoCivil;
+//	}
+//
+//	public void setEstadoCivil(String estadoCivil) {
+//		this.estadoCivil = estadoCivil;
+//	}
+//
+//	public String getConjuge() {
+//		return conjuge;
+//	}
+//
+//	public void setConjuge(String conjuge) {
+//		this.conjuge = conjuge;
+//	}
+//
+//	public String getRecebidoPor() {
+//		return recebidoPor;
+//	}
+//
+//	public void setRecebidoPor(String recebidoPor) {
+//		this.recebidoPor = recebidoPor;
+//	}
+//
+//	public String getDataBatismo() {
+//		return dataBatismo;
+//	}
+//
+//	public void setDataBatismo(String dataBatismo) {
+//		this.dataBatismo = dataBatismo;
+//	}
+//
+//	public String getProcedencia() {
+//		return procedencia;
+//	}
+//
+//	public void setProcedencia(String procedencia) {
+//		this.procedencia = procedencia;
+//	}
+//
+//	public String getSituacao() {
+//		return situacao;
+//	}
+//
+//	public void setSituacao(String situacao) {
+//		this.situacao = situacao;
+//	}
+//
+//	public String getEndereco() {
+//		return endereco;
+//	}
+//
+//	public void setEndereco(String endereco) {
+//		this.endereco = endereco;
+//	}
+//
+//	public String getBairro() {
+//		return bairro;
+//	}
+//
+//	public void setBairro(String bairro) {
+//		this.bairro = bairro;
+//	}
+//
+//	public String getTelefone() {
+//		return telefone;
+//	}
+//
+//	public void setTelefone(String telefone) {
+//		this.telefone = telefone;
+//	}
 }
